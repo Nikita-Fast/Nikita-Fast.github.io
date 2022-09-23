c6ed1ce19bc4c25b55d0a1f1aa456ea73967041b
Nikita Fast <N1kita.fast@yandex.ru>
2022-09-23 17:56:10 +0300

    page

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
+* Start Bootstrap - Freelancer v7.0.6 (https://Nikita-Fast.github.io)
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
